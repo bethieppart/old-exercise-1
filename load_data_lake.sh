a9ae2b5cfb771ec72efe2539b0ccf835b4e77825
bethieppart <bethieppart@gmail.com>
2016-02-28 11:42:17 -0800

    Update load_data_lake.sh

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -6,7 +6,7 @@ tail -n +2 Hospital.General.Information.csv > hospitals.csv
 tail -n +2 Timely.and.Effective.Care.-.Hospital.csv > effective.csv
 tail -n +2 Readmissions.and.Deaths.-.Hospital.csv > readmissions.csv
 tail -n +2 Measure.Dates.csv > measures.csv
-tail -n +2 hvbp_hcahps_05_28_2015.csv surveys.csv
+tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys.csv
 hdfs dfs -mkdir /user/w205/hospital_compare
 hdfs dfs -put hospitals.csv /user/w205/hospital_compare
 hdfs dfs -put effective.csv /user/w205/hospital_compare
