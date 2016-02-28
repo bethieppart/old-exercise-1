4ed3446da69e07f4afa72b948082dd63fe5a5d64
bethieppart <bethieppart@gmail.com>
2016-02-28 14:39:45 -0800

    Update load_data_lake.sh

--- load_data_lake.sh
+++ load_data_lake.sh
@@ -1,3 +1,4 @@
+rm -rf /data/exercise_1/w205_exercise_1
 git clone https://github.com/bethieppart/w205_exercise_1.git
 cd w205_exercise_1
 gunzip Timely.and.Effective.Care.-.Hospital.csv.gz
@@ -6,9 +7,15 @@ tail -n +2 Timely.and.Effective.Care.-.Hospital.csv > effective.csv
 tail -n +2 Readmissions.and.Deaths.-.Hospital.csv > readmissions.csv
 tail -n +2 Measure.Dates.csv > measures.csv
 tail -n +2 hvbp_hcahps_05_28_2015.csv > surveys.csv
+hdfs dfs -rm -r -f /user/w205/hospital_compare1
 hdfs dfs -mkdir /user/w205/hospital_compare1
-hdfs dfs -put hospitals.csv /user/w205/hospital_compare1
-hdfs dfs -put effective.csv /user/w205/hospital_compare1
-hdfs dfs -put readmissions.csv /user/w205/hospital_compare1
-hdfs dfs -put measures.csv /user/w205/hospital_compare1
-hdfs dfs -put surveys.csv /user/w205/hospital_compare1
+hdfs dfs -mkdir /user/w205/hospital_compare1/hdfshospitals
+hdfs dfs -mkdir /user/w205/hospital_compare1/hdfseffective
+hdfs dfs -mkdir /user/w205/hospital_compare1/hdfsreadmissions
+hdfs dfs -mkdir /user/w205/hospital_compare1/hdfsmeasures
+hdfs dfs -mkdir /user/w205/hospital_compare1/hdfssurveys
+hdfs dfs -put hospitals.csv /user/w205/hospital_compare1/hdfshospitals
+hdfs dfs -put effective.csv /user/w205/hospital_compare1/hdfseffective
+hdfs dfs -put readmissions.csv /user/w205/hospital_compare1/hdfsreadmissions
+hdfs dfs -put measures.csv /user/w205/hospital_compare1/hdfsmeasures
+hdfs dfs -put surveys.csv /user/w205/hospital_compare1/hdfssurveys
